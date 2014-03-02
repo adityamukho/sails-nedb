52bf2a60ae9303908390d87f58521cfbaee4e5b5
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2014-03-02 14:57:19 +0530

    Register coll, describe, define.

--- README.md
+++ README.md
@@ -1,75 +1,13 @@
 ![image_squidhome@2x.png](http://i.imgur.com/RIvu9.png) 
 
-# sails-adapter-boilerplate
+# sails-nedb
 
-This template exists to make it easier for you to get started writing an adapter for Sails.js.
+Tested with Sails 0.10
 
-> ### Generator now available
->
-> `$ sails generate adapter foo`
->
-> (see https://github.com/balderdashy/sails-generate-adapter for the source.)
+A persistent object store which uses [Node Embedded Database](https://github.com/louischatriot/nedb) as the storage engine.
 
+## About Sails.js
+http://sailsjs.com
 
-
-
-> ### WARNING
->
-> This version of the adapter is for the upcoming v0.10 release of Sails / Waterline.
-> Check out the 0.8 branch for the original stuff.
-
-
-
-## Getting started
-It's usually pretty easy to add your own adapters for integrating with proprietary systems or existing open APIs.  For most things, it's as easy as `require('some-module')` and mapping the appropriate methods to match waterline semantics.  To get started:
-
-1. Fork this repository
-2. Set up your `README.md` and `package.json` file.  Sails.js adapter module names are of the form sails-*, where * is the name of the datastore or service you're integrating with.
-3. Build your adapter.
-
-## How to test your adapter
-
-Configure the interfaces you plan to support (and targeted version of Sails/Waterline) in the adapter's `package.json` file:
-
-```javascript
-{
-  //...
-  "sailsAdapter": {
-    "sailsVersion": "~0.10.0",
-    "implements": [
-      "semantic",
-      "queryable",
-      "associations"
-    ]
-  }
-}
-```
-
-In your adapter's directory, run:
-
-```sh
-$ npm test
-```
-
-
-## Publish your adapter
-
-> You're welcome to write proprietary adapters and use them any way you wish--
-> these instructions are for releasing an open-source adapter.
-
-1. Create a [new public repo](https://github.com/new) and add it as a remote (`git remote add origin git@github.com:yourusername/sails-youradaptername.git)
-2. Make sure you attribute yourself as the author and set the license in the package.json to "MIT".
-3. Run the tests one last time.
-4. Do a [pull request to sails-docs](https://github.com/balderdashy/sails-docs/compare/) adding your repo to `data/adapters.js`.  Please let us know about any special instructions for usage/testing. 
-5. We'll update the documentation with information about your new adapter
-6. Then everyone will adore you with lavish praises.  Mike might even send you jelly beans.
-
-7. Run `npm version patch`
-8. Run `git push && git push --tags`
-9. Run `npm publish`
-
-## About Sails.js and Waterline
-http://sailsjs.org
-
-Waterline is a new kind of storage and retrieval engine for Sails.js.  It provides a uniform API for accessing stuff from different kinds of databases, protocols, and 3rd party APIs.  That means you write the same code to get users, whether they live in mySQL, LDAP, MongoDB, or Facebook.
-
+## About Waterline
+Waterline is a new kind of storage and retrieval engine.  It provides a uniform API for accessing stuff from different kinds of databases, protocols, and 3rd party APIs.  That means you write the same code to get users, whether they live in mySQL, LDAP, MongoDB, or Facebook.
--- index.js
+++ index.js
@@ -1,193 +1,146 @@
 /**
  * Module Dependencies
  */
-// ...
-// e.g.
-// var _ = require('lodash');
-// var mysql = require('node-mysql');
-// ...
+var async = require('async'),
+  _ = require('lodash'),
+  Datastore = require('nedb'),
+  path = require('path'),
+  fs = require('fs');
 
-
-
-/**
- * Sails Boilerplate Adapter
- *
- * Most of the methods below are optional.
- * 
- * If you don't need / can't get to every method, just implement
- * what you have time for.  The other methods will only fail if
- * you try to call them!
- * 
- * For many adapters, this file is all you need.  For very complex adapters, you may need more flexiblity.
- * In any case, it's probably a good idea to start with one file and refactor only if necessary.
- * If you do go that route, it's conventional in Node to create a `./lib` directory for your private submodules
- * and load them at the top of the file with other dependencies.  e.g. var update = `require('./lib/update')`;
- */
 module.exports = (function () {
-
-
-  // You'll want to maintain a reference to each collection
-  // (aka model) that gets registered with this adapter.
-  var _modelReferences = {};
-
-
-  
-  // You may also want to store additional, private data
-  // per-collection (esp. if your data store uses persistent
-  // connections).
-  //
-  // Keep in mind that models can be configured to use different databases
-  // within the same app, at the same time.
-  // 
-  // i.e. if you're writing a MariaDB adapter, you should be aware that one
-  // model might be configured as `host="localhost"` and another might be using
-  // `host="foo.com"` at the same time.  Same thing goes for user, database, 
-  // password, or any other config.
-  //
-  // You don't have to support this feature right off the bat in your
-  // adapter, but it ought to get done eventually.
-  // 
-  // Sounds annoying to deal with...
-  // ...but it's not bad.  In each method, acquire a connection using the config
-  // for the current model (looking it up from `_modelReferences`), establish
-  // a connection, then tear it down before calling your method's callback.
-  // Finally, as an optimization, you might use a db pool for each distinct
-  // connection configuration, partioning pools for each separate configuration
-  // for your adapter (i.e. worst case scenario is a pool for each model, best case
-  // scenario is one single single pool.)  For many databases, any change to 
-  // host OR database OR user OR password = separate pool.
-  var _dbPools = {};
-
-
+  var modelReferences = {},
+    dbs = {},
+    schemaStash = {};
 
   var adapter = {
-
-    // Set to true if this adapter supports (or requires) things like data types, validations, keys, etc.
-    // If true, the schema for models using this adapter will be automatically synced when the server starts.
-    // Not terribly relevant if your data store is not SQL/schemaful.
     syncable: false,
-
-
-    // Default configuration for collections
-    // (same effect as if these properties were included at the top level of the model definitions)
     defaults: {
-
-      // For example:
-      // port: 3306,
-      // host: 'localhost',
-      // schema: true,
-      // ssl: false,
-      // customThings: ['eh']
-
-      // If setting syncable, you should consider the migrate option, 
-      // which allows you to set how the sync will be performed.
-      // It can be overridden globally in an app (config/adapters.js)
-      // and on a per-model basis.
-      // 
-      // IMPORTANT:
-      // `migrate` is not a production data migration solution!
-      // In production, always use `migrate: safe`
-      //
-      // drop   => Drop schema and data, then recreate it
-      // alter  => Drop/add columns as necessary.
-      // safe   => Don't change anything (good for production DBs)
+      filePath: '.tmp',
       migrate: 'alter'
     },
 
-
-
     /**
-     * 
+     *
      * This method runs when a model is initially registered
      * at server-start-time.  This is the only required method.
-     * 
+     *
      * @param  {[type]}   collection [description]
      * @param  {Function} cb         [description]
      * @return {[type]}              [description]
      */
-    registerCollection: function(collection, cb) {
-
-      // Keep a reference to this collection
-      _modelReferences[collection.identity] = collection;
-      
+    registerCollection: function (collection, cb) {
+      modelReferences[collection.identity] = collection;
+      schemaStash[collection.identity] = collection.definition;
       cb();
     },
-
 
     /**
      * Fired when a model is unregistered, typically when the server
      * is killed. Useful for tearing-down remaining open connections,
      * etc.
-     * 
+     *
      * @param  {Function} cb [description]
      * @return {[type]}      [description]
      */
-    teardown: function(cb) {
+    teardown: function (cb) {
       cb();
     },
 
-
-
     /**
-     * 
+     *
      * REQUIRED method if integrating with a schemaful
      * (SQL-ish) database.
-     * 
+     *
      * @param  {[type]}   collectionName [description]
      * @param  {[type]}   definition     [description]
      * @param  {Function} cb             [description]
      * @return {[type]}                  [description]
      */
-    define: function(collectionName, definition, cb) {
+    define: function (collectionName, definition, cb) {
+      dbs[collectionName] = {
+        // Access db ops through a queue to avoid callstack overflows under heavy loads
+        queue: async.queue(function (payload, done, cursor) {
+          var db = dbs[collectionName].db;
+          if (!cursor) {
+            payload.args.push(done);
+            db[payload.query].apply(db, payload.args);
+          } else {
+            done(db[payload.query].apply(db, payload.args));
+          }
+        }, 1),
+        db: new Datastore({
+          filename: path.join(this.config.filePath, collectionName + '.nedb');
+        })
+      };
+      dbs[collectionName].db.loadDatabase(function (err) {
+        if (err) {
+          return cb(err);
+        }
 
-      // If you need to access your private data for this collection:
-      var collection = _modelReferences[collectionName];
+        // Clone the definition
+        var def = _.clone(definition);
 
-      // Define a new "table" or "collection" schema in the data store
-      cb();
+        function processKey(key, cb) {
+          if (def[key].autoIncrement) {
+            delete def[key].autoIncrement;
+          }
+
+          if (def[key].unique || def[key].index) {
+            return dbs[collectionName].db.ensureIndex({
+              fieldName: key,
+              sparse: true,
+              unique: def[key].unique
+            }, function (err) {
+              if (err) return cb(err);
+              def[key].indexed = true;
+              cb();
+            });
+          }
+
+          cb();
+        }
+
+        var keys = Object.keys(def);
+
+        // Loop through the def and process attributes for each key
+        async.errach(keys, processKey, function (err) {
+          if (err) return cb(err);
+          modelReferences[collectionName].schema = def;
+          cb(null, modelReferences[collectionName].schema);
+        });
+      });
     },
 
     /**
      *
      * REQUIRED method if integrating with a schemaful
      * (SQL-ish) database.
-     * 
+     *
      * @param  {[type]}   collectionName [description]
      * @param  {Function} cb             [description]
      * @return {[type]}                  [description]
      */
-    describe: function(collectionName, cb) {
-
-      // If you need to access your private data for this collection:
-      var collection = _modelReferences[collectionName];
-
-      // Respond with the schema (attributes) for a collection or table in the data store
-      var attributes = {};
-      cb(null, attributes);
+    describe: function (collectionName, cb) {
+      var des = Object.keys(modelReferences[collectionName].schema).length === 0 ?
+        null : modelReferences[collectionName].schema;
+      return cb(null, des);
     },
-
 
     /**
      *
      *
      * REQUIRED method if integrating with a schemaful
      * (SQL-ish) database.
-     * 
+     *
      * @param  {[type]}   collectionName [description]
      * @param  {[type]}   relations      [description]
      * @param  {Function} cb             [description]
      * @return {[type]}                  [description]
      */
-    drop: function(collectionName, relations, cb) {
-      // If you need to access your private data for this collection:
-      var collection = _modelReferences[collectionName];
-
-      // Drop a "table" or "collection" schema from the data store
-      cb();
+    drop: function (collectionName, relations, cb) {
+      delete dbs[collectionName];
+      fs.unlink(path.join(this.config.filePath, collectionName + '.nedb'), cb);
     },
-
-
-
 
     // OVERRIDES NOT CURRENTLY FULLY SUPPORTED FOR:
     // 
@@ -198,52 +151,70 @@ module.exports = (function () {
     // addIndex: function(indexName, options, cb) {},
     // removeIndex: function(indexName, options, cb) {},
 
-
-
     /**
-     * 
+     *
      * REQUIRED method if users expect to call Model.find(), Model.findOne(),
      * or related.
-     * 
+     *
      * You should implement this method to respond with an array of instances.
      * Waterline core will take care of supporting all the other different
      * find methods/usages.
-     * 
+     *
      * @param  {[type]}   collectionName [description]
      * @param  {[type]}   options        [description]
      * @param  {Function} cb             [description]
      * @return {[type]}                  [description]
      */
-    find: function(collectionName, options, cb) {
-
-      // If you need to access your private data for this collection:
-      var collection = _modelReferences[collectionName];
-
+    find: function (collectionName, options, cb) {
       // Options object is normalized for you:
       // 
       // options.where
       // options.limit
       // options.skip
       // options.sort
-      
+
       // Filter, paginate, and sort records from the datastore.
       // You should end up w/ an array of objects as a result.
       // If no matches were found, this will be an empty array.
 
       // Respond with an error, or the results.
-      cb(null, []);
+      var payload = {
+        query: 'find',
+        args: [options.where]
+      },
+        useCursor = _.any(options, function (opt, key) {
+          return (key !== 'where') && !_.isEmpty(opt);
+        });
+
+      if (!useCursor) {
+        dbs[collectionName].queue.push(payload, cb);
+      }
+      else {
+        dbs[collectionName].queue.push(payload, function(cursor) {
+          if (options.sort) {
+            cursor = cursor.sort(options.sort);
+          }
+          if (options.skip) {
+            cursor = cursor.sort(options.skip);
+          }
+          if (options.limit) {
+            cursor = cursor.sort(options.limit);
+          }
+          cursor.exec(cb);
+        }, true);
+      }
     },
 
     /**
      *
      * REQUIRED method if users expect to call Model.create() or any methods
-     * 
+     *
      * @param  {[type]}   collectionName [description]
      * @param  {[type]}   values         [description]
      * @param  {Function} cb             [description]
      * @return {[type]}                  [description]
      */
-    create: function(collectionName, values, cb) {
+    create: function (collectionName, values, cb) {
       // If you need to access your private data for this collection:
       var collection = _modelReferences[collectionName];
 
@@ -253,13 +224,11 @@ module.exports = (function () {
       cb(null, values);
     },
 
-
-
     // 
 
     /**
      *
-     * 
+     *
      * REQUIRED method if users expect to call Model.update()
      *
      * @param  {[type]}   collectionName [description]
@@ -268,7 +237,7 @@ module.exports = (function () {
      * @param  {Function} cb             [description]
      * @return {[type]}                  [description]
      */
-    update: function(collectionName, options, values, cb) {
+    update: function (collectionName, options, values, cb) {
 
       // If you need to access your private data for this collection:
       var collection = _modelReferences[collectionName];
@@ -284,21 +253,20 @@ module.exports = (function () {
       // Respond with error or an array of updated records.
       cb(null, []);
     },
- 
+
     /**
      *
      * REQUIRED method if users expect to call Model.destroy()
-     * 
+     *
      * @param  {[type]}   collectionName [description]
      * @param  {[type]}   options        [description]
      * @param  {Function} cb             [description]
      * @return {[type]}                  [description]
      */
-    destroy: function(collectionName, options, cb) {
+    destroy: function (collectionName, options, cb) {
 
       // If you need to access your private data for this collection:
       var collection = _modelReferences[collectionName];
-
 
       // 1. Filter, paginate, and sort records from the datastore.
       //    You should end up w/ an array of objects as a result.
@@ -311,8 +279,6 @@ module.exports = (function () {
       // Return an error, otherwise it's declared a success.
       cb();
     },
-
-
 
     /*
     **********************************************
@@ -329,7 +295,6 @@ module.exports = (function () {
     // otherwise, uses find() and create()
     findOrCreate: function (collectionName, arrayOfAttributeNamesWeCareAbout, newAttributesObj, cb) { cb(); },
     */
-
 
     /*
     **********************************************
@@ -403,12 +368,9 @@ module.exports = (function () {
 
     */
 
-
   };
-
 
   // Expose adapter definition
   return adapter;
 
 })();
-
